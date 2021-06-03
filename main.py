3024d7d49a4143017c4528ae2a96843dfc464d89
Emanuele <eman_93@hotmail.it>
2021-06-03 10:15:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -9,39 +9,34 @@ from pygame import mixer
 # Initial setup
 
 pygame.init()
-pygame.display.set_caption('Corona Invaders')
-screen = pygame.display.set_mode((1000, 587))
-icon = pygame.image.load('mask.png')
-pygame.display.set_icon(icon)
-bg = pygame.image.load('colosseum.png')
-mixer.music.load('retro.wav')
+pygame.display.set_caption('Corona Invaders') # title
+screen = pygame.display.set_mode((1000, 587)) # Screen
+icon = pygame.image.load('mask.png') # Icon
+pygame.display.set_icon(icon) # display the icon
+bg = pygame.image.load('colosseum.png') # Background imagine
+mixer.music.load('retro.wav') # music
 mixer.music.play(-1)
 
 #fps
-
 fps = 60
 clock = pygame.time.Clock()
 
 # Game over
-
 end_text = pygame.font.Font('freesansbold.ttf', 50)
     
 # Making the flag
-
 flag_cord_X = 460
 flag_cord_Y = 510
 flag_movementX = 0
 flag_imagine = pygame.image.load('italy.png').convert_alpha()
 
 # Making the syringe(bullet)
-
 syringe_cord_X = 0
 syringe_cord_Y = 510
 syringe_loaded = 0
 syringe_imagine = pygame.image.load('vaccine.png').convert_alpha()
 
 # Making Coronavirus
-
 virus = 6
 virus_cord_X = []
 virus_cord_Y = []
@@ -56,28 +51,23 @@ for x in range(virus):
     virus_movement_Y.append(20)
 
 # Keeping count of destroyed enemies
-
 counter = 0
 counter_X = 830
 counter_Y = 550
 score = pygame.font.Font('freesansbold.ttf', 25)
 
 # Display the flag
-
 def flag(Xcordinate, Ycordinate):
     
     screen.blit(flag_imagine, (Xcordinate, Ycordinate))
 
 # Fire the syringe
-
 def syringe(Xcordinate, Ycordinate):
-    
     global syringe_loaded
     syringe_loaded = 1
     screen.blit(syringe_imagine, (Xcordinate + 15, Ycordinate + 10))
 
 def strike(virus_cord_X, virus_cord_Y, syringe_cord_X, syringe_cord_Y):
-    
     two_points_distance = math.sqrt(math.pow(virus_cord_X - syringe_cord_X, 2) + (math.pow(virus_cord_Y - syringe_cord_Y, 2)))
     if two_points_distance < 26:
         return True
@@ -85,27 +75,20 @@ def strike(virus_cord_X, virus_cord_Y, syringe_cord_X, syringe_cord_Y):
         return False
 
 def game_over():
-    
     text = end_text.render('GAME OVER!', True, (255, 255, 255))
     screen.blit(text, (340, 260))
 
 # Showing the score
-
 def destroyed(Xcordinate, Ycordinate):
-    
     destroyed = score.render(f'destroyed: {str(counter)}', True, (255, 255, 255))
     screen.blit(destroyed, (Xcordinate, Ycordinate))    
         
 # Display the enemy    
-
 def corona(Xcordinate, Ycordinate, x):
-    
     screen.blit(corona_imagine[x], (Xcordinate, Ycordinate))    
 
-# Coronavirus movements 
-
+# Coronavirus movements
 def corona_movement():
-    
     global syringe_cord_Y, syringe_loaded, virus_movement_X
     global counter
          
@@ -141,9 +124,7 @@ def corona_movement():
         corona(virus_cord_X[x], virus_cord_Y[x], x)    
         
 # Boundaries    
-    
-def boundaries():
-    
+def boundaries(): 
     global flag_cord_X
     
     if flag_cord_X < 0:
